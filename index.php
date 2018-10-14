3a70f7bebe138ca48abc9c532d573fe99ce03bae
vkzhuk <vikt.anmail@gmail.com>
2018-10-14 10:40:06 +0300

    params

--- index.php
+++ index.php
@@ -7,12 +7,38 @@ use function App\Renderer\render;
 
 $app = new Application;
 
-$app->get('/home', function (){
+$app->get('/home', function () {
     return render('home', ['title' => 'Home page']);
 });
 
-$app->post('/', function (){
+$app->post('/', function () {
     return render('index');
+});
+
+$data = [
+    []
+];
+
+$app->get('/dataSort', function ($params) use ($data) {
+    if (array_key_exists('sort', $params)) {
+        list($key, $order) = explode(' ', $params['sort']);
+
+        usort($data, function ($prev, $next) use ($key, $order) {
+            $prevValue = $prev[$key];
+            $nextValue = $next[$key];
+
+            if ($prevValue == $nextValue) {
+                return 0;
+            }
+
+            if ($order == 'desc') {
+                return $prevValue < $nextValue ? 1 : -1;
+            } else if ($order == 'asc') {
+                return $prevValue > $nextValue ? 1 : -1;
+            }
+        });
+    }
+    return json_encode($data);
 });
 
 $app->run();
