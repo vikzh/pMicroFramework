50afe16b0d4fbb4ef357f3cc6a4fe34dbafd5df9
reddowell <vikt.anmail@gmail.com>
2019-01-18 06:43:44 +0300

    linter fix

--- App/Renderer/Renderer.php
+++ App/Renderer/Renderer.php
@@ -6,6 +6,6 @@ use function App\Template\render as templateRender;
 
 function render($filepath, $params = [])
 {
-    $templatepath = 'resources/views'. DIRECTORY_SEPARATOR . $filepath . '.phtml';
+    $templatepath = 'resources/views' . DIRECTORY_SEPARATOR . $filepath . '.phtml';
     return \App\Template\Render($templatepath, $params);
 }
